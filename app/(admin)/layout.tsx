528e57668d072b72a22ce0890da57d84ba3b3ee9
AltQwerty <58779191+AAP1003@users.noreply.github.com>
2025-04-06 20:51:18 +0700

    fix session redirect

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -22,6 +22,7 @@ export default async function RootLayout({
   // if (session.user.role !== "admin") {
   //   redirect("/"); 
   // }
+  
   return (
     <html lang="en">
       <body>{children}</body>
